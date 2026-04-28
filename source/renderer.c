d9321ec9a5006d39b867fcf73868550f9830fb70
irakli <maniaika07@gmail.com>
2026-04-29 03:34:53 +0400

    chore: remove debug printf and unused stdio.h include

--- source/renderer.c
+++ source/renderer.c
@@ -8,7 +8,6 @@ void draw_bars(int* arr, const struct Config* config) {
     float w = (float)config->window_w / config->list_size; 
     float h = (float)config->window_h / config->list_size;
 
-    printf("%d ", config->list_size);
     for (int i = 0; i < config->list_size; i++) {
         DrawRectangle(
             i * w,

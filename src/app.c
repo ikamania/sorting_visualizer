68a102e915e37590143295280545633f4ce9d1f9
irakli <maniaika07@gmail.com>
2026-04-29 00:46:21 +0400

    feat: add raylib window loop in app

--- src/app.c
+++ src/app.c
@@ -1,6 +1,16 @@
 #include "app.h"
+#include <raylib.h>
 
 
 void run_app(void) {
-    // nothing yeat
+    InitWindow(800, 600, "Sorting Visualizer");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE); // fill background with white
+        EndDrawing();
+    }
+
+    CloseWindow();
 }

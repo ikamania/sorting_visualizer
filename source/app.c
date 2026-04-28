d1e99cefa022694b05df990e90fbbcf8ec7ef9e9
irakli <maniaika07@gmail.com>
2026-04-29 01:27:33 +0400

    feat: load application settings from config in app startup

--- source/app.c
+++ source/app.c
@@ -1,9 +1,12 @@
 #include "app.h"
+#include "config.h"
 #include <raylib.h>
 
 
 void run_app(void) {
-    InitWindow(800, 600, "Sorting Visualizer");
+    struct Config config = load_config("config/config.ini");
+
+    InitWindow(config.window_w, config.window_h, "Sorting Visualizer");
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {

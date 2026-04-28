a79d842b57d27e65e256f17e87349c29e04b9b4a
irakli <maniaika07@gmail.com>
2026-04-29 03:30:38 +0400

    feat: add renderer module to draw array of size n as vertical bars

---
+++ source/renderer.c
@@ -0,0 +1,21 @@
+#include "renderer.h"
+#include "config.h"
+#include <raylib.h>
+#include <stdio.h>
+
+
+void draw_bars(int* arr, const struct Config* config) {
+    float w = (float)config->window_w / config->list_size; 
+    float h = (float)config->window_h / config->list_size;
+
+    printf("%d ", config->list_size);
+    for (int i = 0; i < config->list_size; i++) {
+        DrawRectangle(
+            i * w,
+            config->window_h - arr[i] * h,
+            w,
+            h * arr[i],
+            config->li_color
+        );
+    }
+}

7be25a4436d6816f3cc88a377548f5701be4d51c
irakli <maniaika07@gmail.com>
2026-05-02 01:41:47 +0400

    feat: update renderer to support Pair-based highlighting for sorting visualization

--- source/renderer.c
+++ source/renderer.c
@@ -1,19 +1,26 @@
 #include "renderer.h"
 #include "config.h"
+#include "pair.h"
+
 #include <raylib.h>
 
 
-void draw_bars(int* arr, const struct Config* config) {
+void draw_bars(int* arr, const struct Config* config, struct Pair* pair) {
     float w = (float)config->window_w / config->list_size; 
     float h = (float)config->window_h / config->list_size;
 
     for (int i = 0; i < config->list_size; i++) {
+        Color color = config->li_color;
+
+        if (i == pair->f) color = pair->f_c;
+        if (i == pair->s) color = pair->s_c;
+
         DrawRectangle(
             i * w,
             config->window_h - arr[i] * h,
             w,
             h * arr[i] + 10,
-            config->li_color
+            color
         );
     }
 }

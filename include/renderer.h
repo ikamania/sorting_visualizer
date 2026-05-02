516fe61108d0313ba945b0b4896ae7c983720fa1
irakli <maniaika07@gmail.com>
2026-05-03 02:40:37 +0400

    feat: add visual highlighting for compared elements in renderer

--- include/renderer.h
+++ include/renderer.h
@@ -2,7 +2,8 @@
 #define RENDERER_H
 
 #include "config.h"
+#include "pair.h"
 
-void draw_bars(int* arr, const struct Config* config);
+void draw_bars(int* arr, const struct Config* config, struct Pair* pair);
 
 #endif
--- source/renderer.c
+++ source/renderer.c
@@ -3,17 +3,22 @@
 #include <raylib.h>
 
 
-void draw_bars(int* arr, const struct Config* config) {
+void draw_bars(int* arr, const struct Config* config, struct Pair* pair) {
     float w = (float)config->window_w / config->list_size; 
     float h = (float)config->window_h / config->list_size;
 
     for (int i = 0; i < config->list_size; i++) {
+        Color color = config->li_color;
+
+        if (i == pair->a) color = pair->color_a;
+        if (i == pair->b) color = pair->color_b;
+
         DrawRectangle(
             i * w,
             config->window_h - arr[i] * h,
             w,
             h * arr[i] + 10,
-            config->li_color
+            color
         );
     }
 }
